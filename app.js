a700463aa069fa02e5c06357242700db91978664
BojanIstvancic <bojan.istvancic@gmail.com>
2023-03-31 13:36:31 +0200

    add connect to db

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const connectDB = require("./db/connect.js");
+
 const authRouter = require("./routes/auth");
 const postsRouter = require("./routes/posts");
 const teachersRouter = require("./routes/teachers");
@@ -22,6 +24,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     // add connect to db here later
+    await connectDB(process.env.MONGO_URI);
 
     app.listen(port, () => {
       console.log(`Server is listening on port ${port}`);
